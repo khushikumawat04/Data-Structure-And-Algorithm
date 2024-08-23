bb88443831d4a916e67dd63f8d9c8f80eb342874
khushikumawat04 <kkhushikumawat04@gmail.com>
2024-08-23 09:04:13 +0530

    rotate array

--- Problems on Array/Rotate an array by k element.cpp
+++ Problems on Array/Rotate an array by k element.cpp
@@ -1,32 +1,55 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+// .....1st approch - time complexity -O(n) space complexity - O(n);
 void rotateArray(vector<int> &arr, int n,int k){
-	k = k%n;
-vector <int> temparray(n);
-for(int i=0;i<k;i++){
-	temparray[i]= arr[n-k+i];
+    k = k%n;
+     vector <int> temparray(n);
+    for(int i=0;i<k;i++){
+    	temparray[i]= arr[n-k+i];
+    }
+    for(int i=k; i<n;i++){
+    	temparray[i] = arr[i-k];
+    }
+    
+    for(int i=0; i<n; i++){
+    	arr[i] = temparray[i];
+    }	
+    }
+// 2nd approch ---timecomplexity-O(n) space complexity-o(1)
+void reverse(vector <int> &arr, int start,int end){
+
+    while(start<end){
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
 }
-for(int i=k; i<n;i++){
-	temparray[i] = arr[i-k];
+void Rotatearray(vector<int> &arr, int n, int k){
+    k = k%n;
+    reverse(arr,0,k-1);
+    reverse(arr,k,n-1);
+    reverse(arr,0,n-1);
 }
 
-for(int i=0; i<n; i++){
-	arr[i] = temparray[i];
-}	
-}
+    
 int main(){
-	vector<int> arr = {1, 2, 3, 4, 5, 6, 7};
+	vector<int> arr = {1, 2, 3, 4, 5};
 	int n= arr.size();
 	int k=3;
+	cout<<endl<<" Before rotate"<<endl;
 	for(int i=0; i<n; i++){
 cout<<arr[i]<<" ";
 }
 rotateArray(arr,n,k);
-cout<<endl<<"after rotate";
+// Rotatearray(arr,n,k);
+cout<<endl<<" After rotate"<<endl;
 	for(int i=0; i<n; i++){
 cout<<arr[i]<<" ";
 }
-	
+
+
 	
 }
